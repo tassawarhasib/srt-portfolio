0bd62f6af157ff45148532769daacda98b7f3766
Tassawar hasib <nabilhasib840@gmail.com>
2025-03-11 12:25:30 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-import style from './App.module.css';
-import {About} from './components/About/About';
-import {Contact} from './components/Contact/Contact';
-import {Experience} from './components/Experience/Experience';
-import {Hero} from './components/Hero/Hero';
-import {Navbar} from './components/Navbar/Navbar';
-import {Projects} from './components/Projects/Projects';
-
-
+import styles from './App.module.css';
+import About from "./components/About/About";
+import Contact from "./components/Contact/contact";
+import { Experience } from "./components/Experience/Experience";
+import Footer from "./components/Footer/Footer";
+import Hero from "./components/Hero/Hero";
+import Navbar from "./components/Navbar/Navbar";
+import Project from "./components/Projects/Projects";
 
 function App() {
-
   return (
-    <div className={style.App}>
-    <Navbar/>
-    <Hero/>
-    <About/>
-    <Experience/>
-    <Projects/>
-    <Contact/>
+    <div className={styles.App}>
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+      <Project />
+      <Contact />
+      <Footer />
     </div>
   )
 }
